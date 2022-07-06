97796c4dbb516224cbf7c2140a43ff7974a1e027
nikolaevich23 <in1975@ya.ru>
2022-07-06 09:17:28 +0400

    3.1.2

--- PS3HEN/Make_PKG/rco/EXTR.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd Rcomage
-for %%i in (F:\rco\1\*.rco) do (
-md %%~pi%%~ni
-rcomage.exe dump %%i %%~pi%%~ni.xml --RESDIR %%~pi%%~ni --images - --sounds -
-)
--- PS3HEN/c.bat
+++ PS3HEN/c.bat
@@ -1,8 +1,2 @@
 @echo off
-setlocal enabledelayedexpansion
-
-for %%A in (PS3HEN.BIN_CEX_48?) do (
-set n=%%A
-set v=!n:~17,1!
-xcopy /Y !n! .\Make_PKG\4.8!v!\dev_hdd0\hen\PS3HEN.BIN
-)
+xcopy /y PS3HEN.BIN_CEX_489 .\Make_PKG\4.89.4\dev_hdd0\hen\PS3HEN.BIN
