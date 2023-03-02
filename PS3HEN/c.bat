2c71ecf9b20fddf20360988f1ed4a65473ac073f
nikolaevich23 <in1975@ya.ru>
2023-03-03 00:11:52 +0400

    t

--- PS3HEN/c.bat
+++ PS3HEN/c.bat
@@ -4,5 +4,5 @@ setlocal enabledelayedexpansion
 for %%A in (PS3HEN.BIN_CEX_4??) do (
 set a=%%A
 set b=!a:~16,3!
-xcopy /Y PS3HEN.BIN_CEX_4!b! C:\PS3HEN-320\Make_PKG\4.!b!\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_4!b! C:\PS3HEN\Make_PKG\4.!b!\dev_rewrite\hen\PS3HEN.BIN
 )
