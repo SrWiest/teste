45c960ae469d961cb69d7c2c290ff0f49e27001e
nikolaevich23 <in1975@ya.ru>
2024-03-17 03:58:07 +0400

    -

--- PS3HEN/docs/MinecraftBetterDs3-master/MinecraftBetterDs3/Variables.h
+++
@@ -1,5 +0,0 @@
-namespace msg
-{
-	const char* stopMessage = "Thanks for using!. Subscribe to my YouTube: @destructorrpy\nAll Credits Belongs to their respective owners.\nGitHub:https://github.com/Dsj0036/MinecraftBetterDs3";
-	const char* bootMessage = "";
-}
--- PS3HEN/docs/MinecraftBetterDs3-master/README.md
+++
@@ -1,4 +0,0 @@
-# Minecraft Better Dualshock 3 
-PS3 Sprx to enable more functions to the controller for the Minecraft game also adding better boot mods like FOV, JumpAnim, and similar.
-Button offsets by Misaki.
-Base by me.
