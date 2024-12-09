3fe41e36942457971e13aac80f34a08ea944c207
nikolaevich23 <in1975@ya.ru>
2024-12-09 05:59:33 +0400

    up wm

---
+++ PS3HEN/c-cex_wm.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+
+xcopy /Y PS3HEN.BIN_CEX_491 C:\PS3HEN-master\Make_PKG\4.91wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_491 C:\PS3HEN-master\Make_PKG\4.91wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_491 C:\Github\nikolaevich23.github.io\wm\4.91\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_490 C:\PS3HEN-master\Make_PKG\4.90wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_490 C:\PS3HEN-master\Make_PKG\4.90wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_490 C:\Github\nikolaevich23.github.io\wm\4.90\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_489 C:\PS3HEN-master\Make_PKG\4.89wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_489 C:\PS3HEN-master\Make_PKG\4.89wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_489 C:\Github\nikolaevich23.github.io\wm\4.89\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_488 C:\PS3HEN-master\Make_PKG\4.88wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_488 C:\PS3HEN-master\Make_PKG\4.88wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_488 C:\Github\nikolaevich23.github.io\wm\4.88\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_487 C:\PS3HEN-master\Make_PKG\4.87wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_487 C:\PS3HEN-master\Make_PKG\4.87wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_487 C:\Github\nikolaevich23.github.io\wm\4.87\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_486 C:\PS3HEN-master\Make_PKG\4.86wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_486 C:\PS3HEN-master\Make_PKG\4.86wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_486 C:\Github\nikolaevich23.github.io\wm\4.86\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_485 C:\PS3HEN-master\Make_PKG\4.85wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_485 C:\PS3HEN-master\Make_PKG\4.85wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_485 C:\Github\nikolaevich23.github.io\wm\4.85\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_484 C:\PS3HEN-master\Make_PKG\4.84wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_484 C:\PS3HEN-master\Make_PKG\4.84wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_484 C:\Github\nikolaevich23.github.io\wm\4.84\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_483 C:\PS3HEN-master\Make_PKG\4.83wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_483 C:\PS3HEN-master\Make_PKG\4.83wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_483 C:\Github\nikolaevich23.github.io\wm\4.83\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_482 C:\PS3HEN-master\Make_PKG\4.82wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_482 C:\PS3HEN-master\Make_PKG\4.82wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_482 C:\Github\nikolaevich23.github.io\wm\4.82\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_481 C:\PS3HEN-master\Make_PKG\4.81wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_481 C:\PS3HEN-master\Make_PKG\4.81wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_481 C:\Github\nikolaevich23.github.io\wm\4.81\PS3HEN.p3t
+xcopy /Y PS3HEN.BIN_CEX_480 C:\PS3HEN-master\Make_PKG\4.80wm\dev_rewrite\hen\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_480 C:\PS3HEN-master\Make_PKG\4.80wm\dev_hdd0\hen\mode\release\PS3HEN.BIN
+xcopy /Y PS3HEN.BIN_CEX_480 C:\Github\nikolaevich23.github.io\wm\4.80\PS3HEN.p3t
+
